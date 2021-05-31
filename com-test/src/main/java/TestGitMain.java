8cb23edd367331061695953de082815288ea3b3e
tianliangyu <18372561338@163.com>
2021-05-31 22:50:40 +0800

    测试feature -2

--- com-test/src/main/java/TestGitMain.java
+++ com-test/src/main/java/TestGitMain.java
@@ -8,5 +8,6 @@ public class TestGitMain {
     public static void main(String[] args) {
         System.out.println("这是第一次测试git分支");
         System.out.println("这是第二次测试feature-1分支");
+        System.out.println("这是第三次测试feature-2分支");
     }
 }
