a8dff43dfefb8e55b3869d6fd2147e9bf3f09c47
tianliangyu <18372561338@163.com>
2021-05-31 22:41:37 +0800

    这是第二次测试feature-1

--- com-test/src/main/java/TestGitMain.java
+++ com-test/src/main/java/TestGitMain.java
@@ -5,7 +5,8 @@
  */
 public class TestGitMain {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("这是第一次测试git分支");
+        System.out.println("这是第二次测试feature-1分支");
     }
 }
