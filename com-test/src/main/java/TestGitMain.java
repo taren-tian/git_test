202209720781863437bffcf8558d53b8addb0226
tianliangyu <18372561338@163.com>
2021-05-31 22:27:48 +0800

    这是第一次测试git提交

---
+++ com-test/src/main/java/TestGitMain.java
@@ -0,0 +1,11 @@
+/**
+ * @Description: TODO
+ * @Author : taren.tian
+ * @Date :  2021/5/31  下午10:25
+ */
+public class TestGitMain {
+
+    public static void main(String[] args){
+        System.out.println("这是第一次测试git分支");
+    }
+}
